de5f92f29b93ec137cfa6b1a7f3c7ff43f86c7d8
guifuentess <89105754+guifuentess@users.noreply.github.com>
2022-09-20 09:29:01 -0300

    revisao

---
+++ 2ti/revisao/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revisão</title>
+</head>
+<body>
+    <div>
+        <form action="soma.php" method="post">
+            <label for="n1">Número 1</label>
+            <br>
+            <input type="number" name="n1" id="numero1">
+            <br>
+            <label for="Número2">Número 2</label>
+            <br>
+            <input type="number" name="n2" id="numero2">
+            <br>
+            <input type="submit" value="Calcular">
+        </form>
+    </div>
+</body>
+</html>
---
+++ 2ti/revisao/soma.php
@@ -0,0 +1,6 @@
+<?php
+$num1 = $_POST['n1']
+$num2 = $_POST['n2']
+$resultado = $num1+$num2;
+echo $resultado;
+?>
